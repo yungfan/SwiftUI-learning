34f44ea9494e4b8ed7b3dbe88860efa9933425f5
YungFan <yangfanvw@qq.com>
2022-04-14 19:25:40 +0800

    删除 macOS Demo

--- SwiftUI Tom猫/Tomcat/ContentView.swift
+++ SwiftUI Tom猫/Tomcat/ContentView.swift
@@ -8,18 +8,14 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
-    
-    @State var timer = Timer.publish (every: 0.1, on: .main, in: .common)
-    
+    @State var timer = Timer.publish(every: 0.1, on: .main, in: .common)
+
     @State private var index = 0
-    
-    private let images = (0...80).map { UIImage(named: "drink_\($0)")! }
-    
+
+    private let images = (0 ... 80).map { UIImage(named: "drink_\($0)")! }
+
     var body: some View {
-        
         ZStack {
             Image(uiImage: images[index])
                 .resizable()
@@ -28,53 +24,46 @@ struct ContentView: View {
                     perform: { _ in
                         self.index = self.index + 1
                         if self.index > 80 {
-                            self.index = 0 
+                            self.index = 0
                             self.cancelTimer()
                         }
-                }
-            )
-            
+                    }
+                )
+
             HStack {
-                
                 VStack(spacing: 10) {
-                    
                     Image("cymbal").onTapGesture {
-                        
                         self.instantiateTimer()
                         _ = self.timer.connect()
                     }
-                    
+
                     Image("drink").onTapGesture {
-                        
                     }
-                    
+
                     Image("eat")
                 }
-                
+
                 Spacer()
-                
+
                 VStack(spacing: 10) {
-                    
                     Image("fart")
-                    
+
                     Image("pie")
-                    
+
                     Image("scratch")
-                    
                 }
             }
         }
         .edgesIgnoringSafeArea(.top)
     }
-    
-    
+
     func instantiateTimer() {
-        self.timer = Timer.publish (every: 0.1, on: .main, in: .common)
+        timer = Timer.publish(every: 0.1, on: .main, in: .common)
         return
     }
-    
+
     func cancelTimer() {
-        self.timer.connect().cancel()
+        timer.connect().cancel()
         return
     }
 }
