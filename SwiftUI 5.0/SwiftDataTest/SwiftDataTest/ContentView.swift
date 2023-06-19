99289272e77cbf706673a7c1fc52d62ba2499d49
YungFan <yungfan.vip@outlook.com>
2023-06-19 14:54:07 +0800

    Review Code

--- SwiftUI 5.0/SwiftDataTest/SwiftDataTest/ContentView.swift
+++ SwiftUI 5.0/SwiftDataTest/SwiftDataTest/ContentView.swift
@@ -15,6 +15,7 @@ class TodoItem {
     var id: UUID
     var title: String
     var isCompleted: Bool
+
     init(title: String, isCompleted: Bool) {
         id = UUID()
         self.title = title
@@ -22,9 +23,40 @@ class TodoItem {
     }
 }
 
+struct ContentView: View {
+    // 模型上下文，用于操作数据
+    @Environment(\.modelContext) private var modelContext
+    // 4. 查询
+    @Query private var todoItems: [TodoItem]
+    @State private var title: String = ""
+    @State private var isCompleted: Bool = false
+
+    var body: some View {
+        VStack {
+            TextField("请输入标题", text: $title)
+                .textFieldStyle(.roundedBorder)
+
+            Toggle("是否完成", isOn: $isCompleted)
+
+            // 1. 增加
+            Button("保存") {
+                let todoItem = TodoItem(title: title, isCompleted: isCompleted)
+                modelContext.insert(todoItem)
+                do {
+                    try modelContext.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+
+            TodoListView(todoItems: todoItems)
+        }
+        .padding()
+    }
+}
+
 // MARK: - 列表View，展示所有数据
 struct TodoListView: View {
-    // 模型上下文，用于操作数据
     @Environment(\.modelContext) private var modelContext
     let todoItems: [TodoItem]
 
@@ -53,42 +85,6 @@ struct TodoListView: View {
             let todoItem = todoItems[index]
             // 2. 删除
             modelContext.delete(todoItem)
-        }
-    }
-}
-
-struct ContentView: View {
-    // 上下文
-    @Environment(\.modelContext) private var modelContext
-    // 4. 查询
-    @Query(sort: \TodoItem.title, order: .reverse, animation: .spring) private var todoItems: [TodoItem]
-    @State private var title: String = ""
-    @State private var isCompleted: Bool = false
-
-    var body: some View {
-        NavigationStack {
-            VStack {
-                TextField("请输入标题", text: $title)
-                    .textFieldStyle(.roundedBorder)
-
-                Toggle("是否完成", isOn: $isCompleted)
-
-                TodoListView(todoItems: todoItems)
-            }
-            .padding()
-            .navigationTitle("SwiftData")
-            .toolbar {
-                // 1. 增加
-                Button("保存") {
-                    let todoItem = TodoItem(title: title, isCompleted: isCompleted)
-                    modelContext.insert(todoItem)
-                    do {
-                        try modelContext.save()
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
         }
     }
 }
