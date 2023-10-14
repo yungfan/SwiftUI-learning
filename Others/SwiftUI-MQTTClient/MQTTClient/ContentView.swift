3924ada559a63c0dd7686b359ba9497c038f006d
YungFan <yungfan.vip@outlook.com>
2023-10-14 21:56:54 +0800

    MQTT Client Demo

---
+++ Others/SwiftUI-MQTTClient/MQTTClient/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  MQTTClient
+//
+//  Created by 杨帆 on 2023/10/14.
+//
+
+import CocoaMQTT
+import SwiftUI
+
+struct ContentView: View {
+    @State private var connection: Bool = false
+    let mqttClient = CocoaMQTT(clientID: "swift", host: "broker.emqx.io", port: 1883)
+    @State private var sendMessage = ""
+    @State private var receiveMessages = [String]()
+
+    var body: some View {
+        VStack(spacing: 30) {
+            HStack(spacing: 30) {
+                Button("Connect") {
+                    mqttClient.username = ""
+                    mqttClient.password = ""
+                    mqttClient.willMessage = CocoaMQTTMessage(topic: "/will", string: "dieout")
+                    mqttClient.keepAlive = 60
+                    mqttClient.allowUntrustCACertificate = true
+                    connection = mqttClient.connect()
+                    mqttClient.autoReconnect = true
+                }
+                .disabled(connection)
+
+                Button("Disconnect") {
+                    mqttClient.disconnect()
+                    connection = false
+                }
+                .disabled(!connection)
+            }
+
+            TextField("请输入发送内容", text: $sendMessage)
+                .textFieldStyle(.roundedBorder)
+
+            HStack(spacing: 30) {
+                Button("Subscribe") {
+                    mqttClient.subscribe("topic/swiftui")
+                    mqttClient.didReceiveMessage = { _, message, _ in
+                        print("Message received in topic \(message.topic) with payload \(message.string!)")
+                        receiveMessages.insert("\(message.topic): \(message.string!)", at: 0)
+                    }
+                }
+
+                Button("Publish") {
+                    mqttClient.publish("topic/ios", withString: sendMessage)
+                }
+
+                Button("Clear") {
+                    receiveMessages.removeAll()
+                }
+            }
+            .disabled(!connection)
+
+            ScrollView {
+                ForEach(receiveMessages, id: \.self) { receiveMessage in
+                    HStack {
+                        Text(Date.now.formatted(.dateTime.year().month().day().hour().minute().second().locale(Locale(identifier: "zh_Hans_CN")))) +
+                            Text(receiveMessage)
+                            .foregroundStyle(.red)
+
+                        Spacer()
+                    }
+                    .padding(2)
+                }
+
+                Spacer()
+            }
+        }
+        .padding()
+        .task {
+            mqttClient.username = ""
+            mqttClient.password = ""
+            mqttClient.willMessage = CocoaMQTTMessage(topic: "/will", string: "dieout")
+            mqttClient.keepAlive = 60
+            mqttClient.allowUntrustCACertificate = true
+            connection = mqttClient.connect()
+            mqttClient.autoReconnect = true
+
+            try? await Task.sleep(until: .now + .seconds(1), clock: .continuous)
+
+            mqttClient.subscribe("topic/swiftui")
+            mqttClient.didReceiveMessage = { _, message, _ in
+                print("Message received in topic \(message.topic) with payload \(message.string!)")
+                receiveMessages.insert("\(message.topic): \(message.string!)", at: 0)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Others/SwiftUI-MQTTClient/MQTTClient/MQTTClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTTClientApp.swift
+//  MQTTClient
+//
+//  Created by 杨帆 on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct MQTTClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
