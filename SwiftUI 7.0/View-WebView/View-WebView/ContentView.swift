14ff440f78e9c59bf0563a571494a45fd8583130
YungFan <yungfan.vip@outlook.com>
2025-08-06 10:50:28 +0800

    WebView

---
+++ SwiftUI 7.0/View-WebView/View-WebView/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  View-WebView
+//
+//  Created by 杨帆 on 2025/8/6.
+//
+
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    @State var url = URL(string: "https://www.apple.com.cn")
+
+    var body: some View {
+        VStack {
+            WebView(url: url)
+            // 切换URL
+            Button("Apple") {
+                url = URL(string: "https://www.baidu.com")
+            }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI 7.0/View-WebView/View-WebView/View_WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  View_WebViewApp.swift
+//  View-WebView
+//
+//  Created by 杨帆 on 2025/8/6.
+//
+
+import SwiftUI
+
+@main
+struct View_WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
