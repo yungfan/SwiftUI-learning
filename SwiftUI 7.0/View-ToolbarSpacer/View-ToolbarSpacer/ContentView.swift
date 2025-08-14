f3ab7c1672fe67932010aa6613e73c4c52bab6d4
YungFan <yungfan.vip@outlook.com>
2025-08-14 14:51:34 +0800

    ToolbarSpacer

---
+++ SwiftUI 7.0/View-ToolbarSpacer/View-ToolbarSpacer/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  View-ToolbarSpacer
+//
+//  Created by 杨帆 on 2025/08/14  14:50.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Color.orange
+                .ignoresSafeArea()
+                .toolbar {
+                    Toolbar()
+                }
+        }
+    }
+}
+
+struct Toolbar: ToolbarContent {
+    var body: some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+            } label: {
+                Image(systemName: "trash")
+            }
+        }
+
+        // 分割
+        ToolbarSpacer(.fixed, placement: .topBarTrailing)
+
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+            } label: {
+                Image(systemName: "square.and.pencil")
+            }
+        }
+
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+            } label: {
+                Image(systemName: "text.justify")
+            }
+        }
+
+        // 分割
+        ToolbarSpacer(.flexible, placement: .topBarTrailing)
+
+        ToolbarItem(placement: .topBarTrailing) {
+            Button {
+            } label: {
+                Image(systemName: "photo")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI 7.0/View-ToolbarSpacer/View-ToolbarSpacer/View_ToolbarSpacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  View_ToolbarSpacerApp.swift
+//  View-ToolbarSpacer
+//
+//  Created by 杨帆 on 2025/08/14  14:50.
+//
+
+import SwiftUI
+
+@main
+struct View_ToolbarSpacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
